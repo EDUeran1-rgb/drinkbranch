ad53c8916ebd2182016504cbfb9d6ede1f7a34b5
EDUeran1-rgb <EDUeran1@utb.karlskoga.se>
2026-03-09 11:54:51 +0100

    register

--- register.php
+++ register.php
@@ -8,7 +8,7 @@ if(isset($_POST['btn_reg'])){
     $username=$_POST['username'];
     $realname=$_POST['realname'];
     $mail=$_POST['mail'];
-    $password=$_POST['password'];
+    $password=md5($_POST['password']);
     $sql="INSERT INTO tbl_user(username, password, realname, mail) VALUES ('$username', '$password', '$realname', '$mail')";
     $result=mysqli_query($conn, $sql);
     header("Location: register.php?reg=aac7d883b45");
@@ -24,7 +24,7 @@ if(isset($_POST['btn_reg'])){
 <body>
     <?php if(isset($_GET['reg'])): ?>
             <h1>Thank you for registering a user!</h1>
-            <p>The admin have to approve the user befor you can login. A mail will be sent to th registred email when approved.</p>
+            <a href="index.php">Go to homepage</a>
     <?php else: ?>    
     <form action="register.php" method="POST">
         <label for="username">Username</label>
