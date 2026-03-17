5f5eb2ffa31cc364af8eb74b408bbd15af9966da
EDUeran1-rgb <EDUeran1@utb.karlskoga.se>
2026-03-17 15:20:01 +0100

    ny sql

--- drink.sql
+++ drink.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 27 feb 2026 kl 10:47
+-- Tid vid skapande: 17 mars 2026 kl 13:48
 -- Serverversion: 10.4.32-MariaDB
 -- PHP-version: 8.2.12
 
@@ -34,7 +34,7 @@ CREATE TABLE `tbl_drinks` (
   `ingredients` text NOT NULL,
   `recipe` text NOT NULL,
   `alcoholic` tinyint(4) NOT NULL,
-  `rating` float NOT NULL DEFAULT 3 COMMENT '1-5 is rating possibilities'
+  `rating` int(11) NOT NULL DEFAULT 3 COMMENT 'posibilities'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -42,8 +42,32 @@ CREATE TABLE `tbl_drinks` (
 --
 
 INSERT INTO `tbl_drinks` (`id`, `drinkname`, `description`, `ingredients`, `recipe`, `alcoholic`, `rating`) VALUES
-(1, 'Bloody Mary', 'Almost a salad, eh?', '6 cl Vodka\r\n20 cl Tomato juice\r\nSalt & pepper\r\nA dash of Tabasco sauce\r\nStalk of sellery for decoration', 'Pour Tomato juice in tall glass.\r\nAdd vodka.\r\nAdd salt, pepper and Tabasco after your own taste.\r\nGarnish with sellery.', 1, 3),
-(2, 'Atheist revenge', 'Makes you unbeleive.', '10cl Absinthe\r\n10cl Vodka\r\n10cl White rum\r\n10cl Chocolade sauce\r\n4l Coca Cola', 'Mix all stuff, shake into a big bang.\r\nDrink.', 1, 2);
+(2, 'pilk', 'pepsi milk', 'pepsi\r\nmilk', '1 part pepsi 1 part milk', 0, 5),
+(3, 'vatten', 'vatten', 'vatten', 'vatten', 0, 3),
+(6, '1', '1', '1', '1', 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `tbl_reviews`
+--
+
+CREATE TABLE `tbl_reviews` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `drinkid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumpning av Data i tabell `tbl_reviews`
+--
+
+INSERT INTO `tbl_reviews` (`id`, `userid`, `score`, `drinkid`) VALUES
+(38, 1, 5, 2),
+(39, 3, 5, 2),
+(40, 1, 3, 3),
+(43, 8, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -55,8 +79,8 @@ CREATE TABLE `tbl_user` (
   `id` int(11) NOT NULL,
   `username` varchar(32) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `userlevel` int(11) NOT NULL DEFAULT 5,
-  `lastlogin` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `userlevel` int(11) NOT NULL DEFAULT 10,
+  `lastlogin` timestamp NOT NULL DEFAULT current_timestamp(),
   `realname` varchar(80) NOT NULL,
   `mail` varchar(100) NOT NULL,
   `created` timestamp NOT NULL DEFAULT current_timestamp()
@@ -67,7 +91,10 @@ CREATE TABLE `tbl_user` (
 --
 
 INSERT INTO `tbl_user` (`id`, `username`, `password`, `userlevel`, `lastlogin`, `realname`, `mail`, `created`) VALUES
-(1, '', '', 0, '0000-00-00 00:00:00', '', '', '0000-00-00 00:00:00');
+(1, 'Eric', 'e99a18c428cb38d5f260853678922e03', 1000, '2026-02-06 10:02:15', 'Eric Andersson', 'Mailet@mail', '2026-02-06 10:02:15'),
+(3, 'Eric2', 'e99a18c428cb38d5f260853678922e03', 100, '2026-02-06 10:02:15', 'Eric Andersson2', 'mail@Mailet2', '2026-02-06 10:02:15'),
+(4, 'eric3', '20b40d9cacd8e094feebf93b235db3fe', 10, '2026-03-06 09:00:08', 'Eric Andersson3', 'mail@mail3.mail', '2026-03-06 09:00:08'),
+(7, 'Ant', '202cb962ac59075b964b07152d234b70', 10, '2026-03-09 10:59:51', 'Anton', '13232231@gmail.com', '2026-03-09 10:59:51');
 
 --
 -- Index för dumpade tabeller
@@ -77,6 +104,12 @@ INSERT INTO `tbl_user` (`id`, `username`, `password`, `userlevel`, `lastlogin`, 
 -- Index för tabell `tbl_drinks`
 --
 ALTER TABLE `tbl_drinks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index för tabell `tbl_reviews`
+--
+ALTER TABLE `tbl_reviews`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -93,13 +126,19 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT för tabell `tbl_drinks`
 --
 ALTER TABLE `tbl_drinks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT för tabell `tbl_reviews`
+--
+ALTER TABLE `tbl_reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT för tabell `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
